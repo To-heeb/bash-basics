e447e3f86dcc80bb05519ac77df62e2b93535231
Toheeb Oyekola <toheeb.olawale.to23@gmail.com>
2023-03-12 21:13:42 +0100

    select case and infinite loops

---
+++ professional_wait.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Wait for user input
+echo "Press any key to continue..."
+while [ true ]; do
+    # Read every 3 seconds
+    read -t 3 key
+    if [ $? -eq 0 ]; then
+        echo "You have asked to terminate the script."
+    else
+        echo "Waiting for user input, press any key..."
+    fi
+done
